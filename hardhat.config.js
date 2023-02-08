fd1e2c9136a8777c532708f09346db964ed4e72d
qedric <52743971+qedric@users.noreply.github.com>
2023-02-08 23:15:51 +0100

    first commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,23 @@
 /** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
+
+const { ALCHECMY_API_KEY_TEST, ETHERSCAN_API_KEY, PK } = process.env;
+
 module.exports = {
   solidity: {
-    version: '0.8.9',
+    version: '0.8.11',
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200,
+        runs: 1000,
       },
     },
   },
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHECMY_API_KEY_TEST}`,
+      accounts: [PK]
+    }
+  }
 };
