22f0015fef789593e9188f4899aa466043c03c86
Qedric <qedricjames@gmail.com>
2024-01-31 12:43:10 +0100

    added docgen plugin

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,7 @@ require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-etherscan");
 require("hardhat-gas-reporter");
 require('dotenv').config();
+require('solidity-docgen');
 
 const { ALCHECMY_API_KEY_TEST, ALCHEMY_API_KEY_SEPOLIA, ETHERSCAN_API_KEY, PK } = process.env;
 
@@ -36,4 +37,5 @@ module.exports = {
     gasPrice: 20,
     coinmarketcap: '9ce1674f-8587-4207-877a-705d1429764b'
   },
+  docgen: { },
 };
